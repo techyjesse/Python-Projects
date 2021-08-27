bb3ed1ada8b814ad3233fe5f4496236c7f623a2e
Jesse Kuykendall <84601365+techyjesse@users.noreply.github.com>
2021-08-27 11:48:52 -0700

    classes_assignment

---
+++ class_assignment.py
@@ -0,0 +1,18 @@
+
+# parent class that chdclasses will inherit from
+class School:
+    schoolName = ''
+    city = ''
+    state = '
+
+# chd class / inherits from above
+class Teacher(School):
+    name = ''
+    salary = ''
+    department = ''
+
+# chd class / inherits from above
+class Student(School):
+    name = ''
+    age = '' 
+    gpa = ''
