9c208c7e9fbd56603f2daa7634937c89e55c491c
Jesse Kuykendall <84601365+techyjesse@users.noreply.github.com>
2021-09-02 13:35:17 -0700

    AbstractMethod

---
+++ Abstract_method.py
@@ -0,0 +1,18 @@
+
+from abc import ABC, abstractmethod
+class Student(ABC):
+    def name(self, name):
+        print("Student Name: ",name)
+
+    @abstractmethod
+    def sport(self,name):
+        pass
+
+class studentSport(Student):
+
+    def sport(self,name):
+            print('Our student {} is ready to play sportsball!'.format(name))
+
+obj = studentSport()
+obj.name("Jesse")
+obj.sport("Basketball")
