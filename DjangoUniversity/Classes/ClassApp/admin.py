63460b70caeb516d692af843d24d1c4977cfe85b
Jesse Kuykendall <84601365+techyjesse@users.noreply.github.com>
2021-09-25 14:00:03 -0700

    DjangoUniversity Final Commit

--- DjangoUniversity/Classes/ClassApp/admin.py
+++ DjangoUniversity/Classes/ClassApp/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 
 from .models import djangoClasses
 
-admin.register(djangoClasses)
+admin.site.register(djangoClasses)
--- DjangoUniversity/Classes/ClassApp/models.py
+++ DjangoUniversity/Classes/ClassApp/models.py
@@ -1,21 +1,13 @@
-from django.db import migrations, models
+from django.db import models
 
 
 class djangoClasses(models.Model):
-    Title = models.CharField(max_length=100),
-    Course_Number = models.IntegerField(null=True, blank=False),
-    Instructor_Name = models.CharField(max_length=100),
-    Duration = models.FloatField(min=0.0, max=10.0)
+    title = models.CharField(max_length=100, default='')
+    course_Number = models.IntegerField(null=False, default=0)
+    instructor_Name = models.CharField(max_length=100, default='')
+    duration = models.FloatField()
 
     objects = models.Manager()
 
     def __str__(self):
-        return self.Title
-
-    venvCourse = models.objects.create(Title="Venv", Course_Number=1, Instructor_Name="Mr. Kuykendall", Duration=2.5)
-    modelCourse = models.objects.create(Title="Models", Course_Number=2, Instructor_Name="Ms. Quang", Duration=3.25)
-    viewsCourse = models.objects.create(Title="Views", Course_Number=3, Instructor_Name="Mr. Kirkby", Duration=4)
-
-    models.save()
-
-
+        return self.title
